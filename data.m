cb80a8110c53a8c49842b780c52053330f2e77fe
tomdema42 <tom.dema@outlook.it>
2023-07-03 16:51:31 +0200

    v3

--- data.m
+++ data.m
@@ -11,11 +11,12 @@ S_ala = .46*7.3;
 %% Variable DATA (da modificare)
 v_cruise = 13;
 C_D = 0.029; 
+C_Dfriction = C_D;
 C_L = 0.5639;
-attrito = Thrust*0.01; % da definire
+% attrito = Thrust*0.01; % da definire
 %% Computed DATA
 D = 0.5*rho*v_cruise^2*S_ala*C_D;
 Thrust = D;
 L =  0.5*rho*v_cruise^2*S_ala*C_L
-peso = massa*9.8
+peso = massa*9.8;
 CL = peso/(0.5*rho*v_cruise^2*S_ala);
