7e6170ab3e9d9c8bb2682e1abbeddd102be6ab2b
tomdema42 <tom.dema@outlook.it>
2023-07-03 15:48:00 +0200

    v2

--- data.m
+++ data.m
@@ -12,9 +12,10 @@ S_ala = .46*7.3;
 v_cruise = 13;
 C_D = 0.029; 
 C_L = 0.5639;
+attrito = Thrust*0.01; % da definire
 %% Computed DATA
 D = 0.5*rho*v_cruise^2*S_ala*C_D;
 Thrust = D;
 L =  0.5*rho*v_cruise^2*S_ala*C_L
 peso = massa*9.8
-CL = Peso/(0.5*rho*v_cruise^2*S_ala);
+CL = peso/(0.5*rho*v_cruise^2*S_ala);
