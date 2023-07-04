eee9ea9d1a6aa36d224c031431b3515b0aa872f9
Giovy2497 <vanni-pitarresi@hotmail.it>
2023-07-04 17:01:54 +0200

    Aggiunta suolo 0m

--- data.m
+++ data.m
@@ -7,15 +7,17 @@ massa = 20; %kg
 alfa  = deg2rad(3); %angolo di attacco
 rho = 1.225; %densità
 S_ala = .46*7.3;
-
+quota=100;
 %% Variable DATA (da modificare)
 v_cruise = 13;
 C_D = 0.029; 
 C_L = 0.5639;
-attrito = Thrust*0.01; % da definire
+
 %% Computed DATA
 D = 0.5*rho*v_cruise^2*S_ala*C_D;
 Thrust = D;
-L =  0.5*rho*v_cruise^2*S_ala*C_L
-peso = massa*9.8
+L =  0.5*rho*v_cruise^2*S_ala*C_L;
+peso = massa*9.8;
 CL = peso/(0.5*rho*v_cruise^2*S_ala);
+
+attrito = Thrust*0.01; % da definire
