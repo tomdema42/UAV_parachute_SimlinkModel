be5d7ee4b2cb06620b19c252471dac2968c319c6
Giovy2497 <vanni-pitarresi@hotmail.it>
2023-07-05 19:04:12 +0200

    Creazione Blocchi e migliore organizzazione

--- data.m
+++ data.m
@@ -3,29 +3,22 @@ close all
 clc
 
 %% const data
-massa = 20; %kg
+mass = 20; %kg
 alfa  = deg2rad(3); %angolo di attacco
 rho = 1.225; %densità
 S_ala = .46*7.3;
-quota=100;
+altitude=100;
 %% Variable DATA (da modificare)
 v_cruise = 13;
 C_D = 0.029; 
 C_Dfriction = C_D;
 C_L = 0.5639;
+Time_stop_motor=2000;
 
 
 %% Computed DATA
 D = 0.5*rho*v_cruise^2*S_ala*C_D;
 Thrust = D;
 L =  0.5*rho*v_cruise^2*S_ala*C_L;
-peso = massa*9.8;
-CL = peso/(0.5*rho*v_cruise^2*S_ala);
-
-% attrito = Thrust*0.01; % da definire
-%% Computed DATA
-D = 0.5*rho*v_cruise^2*S_ala*C_D;
-Thrust = D;
-L =  0.5*rho*v_cruise^2*S_ala*C_L
-peso = massa*9.8;
-CL = peso/(0.5*rho*v_cruise^2*S_ala);
+weight = mass*9.8;
+CL = weight/(0.5*rho*v_cruise^2*S_ala);
