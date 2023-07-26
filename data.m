b478b7609bfbea5404affad0a59cacab28dfb7b1
Giovy2497 <vanni-pitarresi@hotmail.it>
2023-07-26 10:40:44 +0200

    non ricordo che modifiche

--- data.m
+++ data.m
@@ -32,12 +32,12 @@ CD_parachute = [1.7,1.3,1.10,0.9,0.87,0.85,0.84,0.82,0.81,0.80]; %da modificare 
 V_parachute = linspace(3,12,10);
 diam_parachute = 3.04;% [m] diametro paracadute 30 cm
 S_parachute = pi*(diam_parachute/2)^2;% area cerchio paracadute
-
-sim mod_v1.slx
-clc
-
-plot(ans.Position_x.Data,ans.Position_y.Data)
-xlabel(" X [m]")
-ylabel(" Z [m]")
-grid on
+Time_open_parachute=Time_stop_motor+10;
+% sim mod_v1.slx
+% clc
+% 
+% plot(ans.Position_x.Data,ans.Position_y.Data)
+% xlabel(" X [m]")
+% ylabel(" Z [m]")
+% grid on
 
