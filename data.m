a3f582ee02c986a1be9639e1e506b3f9305d98a3
tomdema42 <tom.dema@outlook.it>
2023-08-14 03:47:01 +0200

    plotter e ON/OFF parachute

--- data.m
+++ data.m
@@ -9,11 +9,13 @@ dt=0.01;
 
 %% const data
 altitude=250;
+parachute_mass=0.250;
 mass = 15; %kg
+total_mass=mass+parachute_mass;
 alfa  = deg2rad(2); %angolo di attacco
 % rho = densita_a_quota(altitude); %densità
 rho=1.1959; % Preso da Simulink
-rho=1.255;
+% rho=1.255;
 S_ala = 1.484; %senza winglet
 
 %% Variable DATA (da modificare)
@@ -31,24 +33,21 @@ v_cruise = sqrt(weight/(0.5*rho*S_ala*CL));
 D = 0.5*rho*v_cruise^2*S_ala*C_D;
 Thrust = D;
 L =  0.5*rho*v_cruise^2*S_ala*CL;
-v_discesa = 5;
+v_discesa = 5.1;
 
-%% polare
-
-polare = readmatrix("polare.csv");
-angoli = linspace(polare(1,1),polare(end,1));
-polare2 = spline(polare(:,1),polare(:,2),angoli);
 
 %% parachute
-% CD_parachute = [1.7,1.3,1.10,0.9,0.87,0.85,0.84,0.82,0.81,0.80]; %da modificare prendendo estrapolazione dati da paper in funzione della veloctià
-% V_parachute = linspace(3,12,10);
-diam_parachute = 2.7;% [m] diametro paracadute 
-chord=2;
 
-S_parachute = pi*(diam_parachute/2)^2;% area cerchio paracadute
-T = 8*diam_parachute/10^.9; % paper Guglieri (il Cirrus si apre in 5 - 7 s)
-Tempo = 4*diam_parachute/(15.5*.85); % paper Knacke (tempo di inflaction) 
-                                     % ci prende addirittura bene (assurdo)
-                                     % Consideriamo Solid Flat Circular Parachute
+diam_parachute = 2.7;% [m] diametro paracadute 
+chord=3.050;
+
+S_parachute = pi*(diam_parachute/2)^2;  % area cerchio paracadute
+T = 8*diam_parachute/10^.9;             % paper Guglieri (il Cirrus si apre in 5 - 7 s)
+Tempo = 4*diam_parachute/(15.5*.85);    % paper Knacke (tempo di inflaction) 
+                                        % ci prende addirittura bene (assurdo)
+                                        % Consideriamo Solid Flat Circular Parachute
 CD_para=weight/(0.5*rho*v_discesa^2*S_parachute);
 
+
+
+add_mass=(0.25+2/3)*pi*(diam_parachute/2)^3;
---
+++ plotter.m
@@ -0,0 +1,90 @@
+%%%% PLOTTER
+
+%% TRAJECTORY out.sim_position
+close all
+time = out.sim_position.Time;
+x = out.sim_position.Data(:,1);
+z  = out.sim_position.Data(:,3);
+figure(1)
+plot3(x,time,z,'k',LineWidth=1.5)
+grid on
+xlabel('x [m]')
+ylabel('time [s]')
+zlabel('z [m]')
+title('UAV Trajectory')
+ylim([80, 160]) 
+%  saveas(figure(1),[pwd '/images/trajectory.png'])
+%% Altitude plot out.sim_position
+figure(2)
+plot(time,z,'k',LineWidth=1.5)
+grid on
+hold on
+xline(Time_stop_motor,'r--',LineWidth=1.5)
+xline(Time_start_parachute,'b--',LineWidth=1.5)
+xlabel('time [s]')
+ylabel('Altitude [m]')
+title('UAV Altitude')
+xlim([80, 160]) 
+legend('','Time stop motor','Time start parachute',Location='best')
+%  saveas(figure(2),[pwd '/images/altitude.png'])
+ %% Forza paracadute out.sim_Fpara
+close all
+time = out.sim_Fpara.Time;
+Fpara = out.sim_Fpara.Data;
+
+figure(3)
+plot(time,Fpara,'k')
+hold on
+xline(Time_stop_motor,'r--',LineWidth=1.5)
+xline(Time_start_parachute,'b--',LineWidth=1.5)
+xlabel('time [s]')
+ylabel('Force [N]')
+title('Force on parachute')
+grid on
+legend('','Time stop motor','Time start parachute',Location='best')
+xlim([80, 160]) 
+% saveas(figure(3),[pwd '/images/force_parachute.png'])
+%% Velocity u & w out.sim_velocity
+close all
+time = out.sim_velocity.Time;
+u = out.sim_velocity.Data(:,1);
+w = out.sim_velocity.Data(:,3);
+figure(4)
+plot(time,u,'k',LineWidth=1.5)
+hold on
+plot(time,w,'Color',[0.8500 0.3250 0.0980],LineWidth=1.5)
+xline(Time_stop_motor,'r--',LineWidth=1.5)
+xline(Time_start_parachute,'b--',LineWidth=1.5)
+xlabel('time [s]')
+ylabel('velocity [m/s]')
+title('Velocity')
+grid on
+xlim([80, 160]) 
+legend('u','w','Time stop motor','Time start parachute',Location='best')
+% saveas(figure(4),[pwd '/images/velocity.png'])
+%% Energy out.sim_energy
+close all 
+% time = out.sim_energy.Time;
+%  E = out.sim_energy.Data(:,1);
+%  time_NOparachute = out.sim_energy_NOpara.Time; %fare Sim togliendo
+%  Parachute
+%  E_NOparachute = out.sim_energy_NOpara.Data(:,1);
+
+%%
+ figure(5)
+plot(time,E,'k',LineWidth=1.5)
+hold on
+plot(time_NOparachute,E_NOparachute,'Color',[0.8500 0.3250 0.0980],LineWidth=1.5)
+
+xline(Time_stop_motor,'r--',LineWidth=1.5)
+xline(Time_start_parachute,'b--',LineWidth=1.5)
+% xline(time(end),'--','Color',[0.8500 0.3250 0.0980],LineWidth=1.5)
+xlabel('time [s]')
+ylabel('Energy [J]')
+grid on
+xlim([80 160])
+title('Kinetic energy')
+legend('Parachute','NO parachute','Time stop motor','Time start parachute',Location='best')
+% saveas(figure(5),[pwd '/images/energy_NOpara.png'])
+
+%%
